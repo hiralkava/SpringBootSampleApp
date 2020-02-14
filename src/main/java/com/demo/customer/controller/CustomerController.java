c0a748a76a90a5fdf2e08f6c9ed77d2f02bf4980
np19027@gmail.com <parshvavora@Parshva-103.local>
2020-02-14 08:23:28 -0800

    Add logging features

--- src/main/java/com/demo/customer/controller/CustomerController.java
+++ src/main/java/com/demo/customer/controller/CustomerController.java
@@ -18,6 +18,7 @@ public class CustomerController {
 	@RequestMapping("/getAllCustomer")
 	public List<Customer> getAllLocation()
 	{
+		System.out.println("Logging... ");
 		return customerService.getAllCustomer();
 	}
 	
