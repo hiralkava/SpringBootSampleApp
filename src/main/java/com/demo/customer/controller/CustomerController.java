541d72ac1bd6daff9cad4e00606b8e4231ad90e1
parshva <parshvavora@Parshva-103.local>
2020-06-07 15:40:08 -0700

    Changing Logging

--- src/main/java/com/demo/customer/controller/CustomerController.java
+++ src/main/java/com/demo/customer/controller/CustomerController.java
@@ -18,7 +18,7 @@ public class CustomerController {
 	@RequestMapping("/getAllCustomer")
 	public List<Customer> getAllLocation()
 	{
-		System.out.println("Logging... Cusomer Data ");
+		System.out.println("Logging... Cusomer ");
 		return customerService.getAllCustomer();
 	}
 	
